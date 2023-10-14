96538dc9b91d13e220c9bb9dc424c5424a52c7c6
Dennis Hilgert <dennis@hilgerts.de>
2023-10-14 15:42:11 +0200

    Add testing mode without real images

--- read-gauge.sh
+++ read-gauge.sh
@@ -6,5 +6,8 @@ img_file_name=gauge-${today}.jpg
 # take picture with raspberry pi camera
 raspistill -o images/${img_file_name}
 
+# for local testing without real images
+#cp images/gauge-23.jpg images/${img_file_name}
+
 # read the value of the analog gauge image
 python3 analog_gauge_reader.py --image images/${img_file_name}
